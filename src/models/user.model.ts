f5abcdb75f6cef48a5b6f252c7b3e3f2242a83c4
Mahdi Alizadeh <mahdi.alizadeh.sp@gmail.com>
2023-03-31 17:15:26 +0330

    add user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,24 @@
+// <import packages
+import mongoose, { Schema } from "mongoose";
+// import packages>
+
+/**
+ * @description create user schema
+ */
+const userSchema = new Schema({
+    email : {
+        type : String,
+        required : true,
+        unique: true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    userName : {
+        type : String,
+        required : false
+    }
+}, {timestamps : true});
+
+export default mongoose.model("User", userSchema);
