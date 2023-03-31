8c5703158d9fc76072064f5a0a9a312a0ea3a4eb
Mahdi Alizadeh <mahdi.alizadeh.sp@gmail.com>
2023-03-31 20:50:19 +0330

    add joi validtion messages

--- src/lib/validations/posts.validation.ts
+++ src/lib/validations/posts.validation.ts
@@ -2,15 +2,30 @@
 import Joi from 'joi';
 // import packages>
 
+// <import messages
+import messages from '../messages/messages.json'
+// import messages>
+
 /**
  * @key createdBy --> body
  * @key quotationsBy --> body
  * @key postContent --> body
  */
 const addPost = Joi.object({ // post validation schema
-    createdBy: Joi.string().required(),
-    quotationsBy: Joi.string().required(),
-    postContent: Joi.string().required().min(5).max(200)
+    createdBy: Joi.string().required().messages({
+                                                ...messages.posts.validation.createdBy.empty,
+                                                ...messages.posts.validation.createdBy.required,
+                                                }),
+    quotationsBy: Joi.string().required().messages({
+                                                ...messages.posts.validation.quotationsBy.empty,
+                                                ...messages.posts.validation.quotationsBy.required,
+                                                }),
+    postContent: Joi.string().required().min(5).max(200).messages({
+                                                                ...messages.posts.validation.postContent.empty,
+                                                                ...messages.posts.validation.postContent.required,
+                                                                ...messages.posts.validation.postContent.max,
+                                                                ...messages.posts.validation.postContent.min,
+                                                                })
 });
 
 /**
@@ -19,16 +34,30 @@ const addPost = Joi.object({ // post validation schema
  * @key id --> query
  */
 const patchPost = Joi.object({ // post validation schema
-    quotationsBy: Joi.string().required(),
-    postContent: Joi.string().required().min(5).max(200),
-    id: Joi.string().required()
+    quotationsBy: Joi.string().required().messages({
+                                                    ...messages.posts.validation.quotationsBy.empty,
+                                                    ...messages.posts.validation.quotationsBy.required,
+                                                    }),
+    postContent: Joi.string().required().min(5).max(200).messages({
+                                                                ...messages.posts.validation.postContent.empty,
+                                                                ...messages.posts.validation.postContent.required,
+                                                                ...messages.posts.validation.postContent.max,
+                                                                ...messages.posts.validation.postContent.min,
+                                                                }),
+    id: Joi.string().required().messages({
+                                        ...messages.posts.validation.id.empty,
+                                        ...messages.posts.validation.id.required
+                                        }),
 });
 
 /**
  * @key id --> query
  */
 const deletePost = Joi.object({ // post validation schema
-    id: Joi.string().required()
+    id: Joi.string().required().messages({
+                                        ...messages.posts.validation.id.empty,
+                                        ...messages.posts.validation.id.required
+                                        }),
 });
 
 export default {
